9a6cf931a5e2e7b9afdc990c23aa2a0a3e79c12e
Mariana <marianacastrorc@gmail.com>
2024-12-04 18:04:11 -0300

    implementing responsive design

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -7,31 +7,33 @@ $validations = flash()->get('validations') ?? [];
 $message = flash()->get('successfully_registered');
 ?>
 
-<div class="bg-base-300 box w-56 rounded-l-box">
-  <div class="bg-base-200 p-4 rounded-tl-box">
-    + Note
-  </div>
-</div>
-
-<div class="w-full bg-base-200 rounded-r-box p-10">
-  <form action="/notes/create" method="post" class="flex flex-col space-y-6">
-    <label class="form-control w-full">
-      <div class="label">
-        <span class="label-text">Title</span>
-      </div>
-      <input name="title" type="text" placeholder="Your title here" class="input input-bordered w-full" />
-      <span class="text-error mt-1 text-sm"><?= getErrors($validations, 'title') ?></span>
-    </label>
-    <label class="form-control">
-      <div class="label">
-        <span class="label-text">Your note</span>
-      </div>
-      <textarea name="note" class="textarea textarea-bordered h-24" placeholder="Your note here"></textarea>
-      <span class="text-error mt-1 text-sm"><?= getErrors($validations, 'note') ?></span>
-    </label>
-
-    <div class="flex items-center justify-end">
-      <button class="btn btn-secondary">Save</button>
+<div class="flex flex-col lg:flex-row gap-4 lg:gap-0 w-full">
+  <div class="bg-base-300 box w-full rounded-box lg:w-56 lg:rounded-tr-none lg:rounded-br-none">
+    <div class="bg-base-200 p-4 rounded-box lg:rounded-b-none lg:rounded-tr-none">
+      + Note
     </div>
-  </form>
+  </div>
+
+  <div class="w-full bg-base-200 rounded-box lg:rounded-l-none p-6 lg:p-10 flex flex-col space-y-6">
+    <form action="/notes/create" method="post" class="flex flex-col space-y-6">
+      <label class="form-control w-full">
+        <div class="label">
+          <span class="label-text">Title</span>
+        </div>
+        <input name="title" type="text" placeholder="Your title here" class="input input-bordered w-full" />
+        <span class="text-error mt-1 text-sm"><?= getErrors($validations, 'title') ?></span>
+      </label>
+      <label class="form-control">
+        <div class="label">
+          <span class="label-text">Your note</span>
+        </div>
+        <textarea name="note" class="textarea textarea-bordered h-24" placeholder="Your note here"></textarea>
+        <span class="text-error mt-1 text-sm"><?= getErrors($validations, 'note') ?></span>
+      </label>
+
+      <div class="flex items-center justify-end">
+        <button class="btn btn-secondary">Save</button>
+      </div>
+    </form>
+  </div>
 </div>
--- views/partials/_navbar.view.php
+++ views/partials/_navbar.view.php
@@ -3,20 +3,22 @@
 use function Core\auth;
 ?>
 <div class="navbar bg-base-100">
-    <div class="flex-1">
-      <a href="/notes" class="btn btn-ghost text-xl">Lockbox</a>
-    </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Link</a></li>
-        <li>
-          <details>
-            <summary><?= auth()->name ?></summary>
-            <ul class="bg-base-100 rounded-t-none p-2">
-              <li><a href="/logout">Logout</a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
-    </div>
+  <div class="flex-1">
+    <a href="/notes" class="btn btn-ghost text-xl">Lockbox</a>
   </div>
+
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1 flex">
+      <li><a>Link</a></li>
+      <li>
+        <details>
+          <summary><?= auth()->name ?></summary>
+          <ul class="bg-base-100 rounded-t-none p-2">
+            <li><a href="/logout">Logout</a></li>
+          </ul>
+        </details>
+      </li>
+    </ul>
+    
+  </div>
+</div>
